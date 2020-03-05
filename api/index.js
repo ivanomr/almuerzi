d9175400560f43890041d053910733082935e851
IVAN MARTINEZ RAMIREZ <IVAN.MARTINEZ@cusaem.gob.mx>
2020-03-05 17:28:40 -0600

    mi primer commit

---
+++ api/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app=express()
+
+mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true,useUnifiedTopology: true })
+
+const Users=mongoose.model('User',new mongoose.Schema({name:String}))
+Users.create({name:'canchito feliz'})
+
+app.get('*',(req,res)=>{
+    Users.find().then(x=>res.send(x))
+})
+
+module.exports=app
